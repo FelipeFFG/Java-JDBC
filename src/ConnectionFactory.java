9810028003402dbedfb436281890da3872d12156
Felipe <felipeffggomes@hotmail.com>
2021-06-15 17:51:40 -0300

    Adicionando Conteudo de Escalabilidade com pool de conexoes

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,11 +1,26 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory { //encapsulamento de codigo para criar uma conxao, facilitando a mudança do codigo caso futuramente decidimos alterar o tipo de banco de dados, sem precisar alterar em todas as classes.
 
+    public DataSource dataSource;
+
+    public ConnectionFactory(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();  //instaciação da pool.
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:8080/loja_virtual?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+
+        comboPooledDataSource.setMaxPoolSize(15);  //limitamos as conexoes.
+
+        this.dataSource = comboPooledDataSource;  //faz com que o nosso pool funcione,e expor as  informações do pool de conexoes
+    }
+
     public Connection recuperarConexao() throws SQLException {
-       return DriverManager.getConnection("jdbc:mysql://localhost:8080/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+       return this.dataSource.getConnection();
     }
 
 }
