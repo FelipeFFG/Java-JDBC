e1cd708f17a9a8a4f819aaeb562a967628331930
Felipe <felipeffggomes@hotmail.com>
2021-06-15 13:15:06 -0300

    Criando Projeto

---
+++ src/TestaConexao.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+    public static void main(String[] args)throws SQLException {
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8080/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+        System.out.println("Fechando");
+        con.close();
+    }
+}
